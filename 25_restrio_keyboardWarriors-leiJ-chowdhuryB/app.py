946b3dc819edb2105fc5aa8f8e7561d47e390e45
Biraj Chowdhury <birajchowdhury@Birajs-MBP.nyc.rr.com>
2019-11-13 22:24:02 -0500

    finished

--- 25_restrio_keyboardWarriors-leiJ-chowdhuryB/app.py
+++ 25_restrio_keyboardWarriors-leiJ-chowdhuryB/app.py
@@ -1,24 +1,66 @@
-from urllib.request import urlopen
+from os import urandom
 from flask import Flask, render_template
-import json
+
+from urllib.request import urlopen
+from json import loads, dumps
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    url = urlopen(
-        "http://api.citybik.es/v2/networks/bikemi"
-        )
-    response = url.read()
-    data = json.loads(response)['network']
-    return render_template('index.html',
-                               company=data['company'][0],
-                               location = data['location']['city']
-                               )
+app.secret_key = urandom(32)
 
-@app.route("/next")
-def nextRoute():
-    url = urlopen(
+@app.route("/")
+def root():
+    return "redirect to /citibike, /ricknmorty, or /collegescorecard"
+
+@app.route("/citibike")
+def citibike():
+    req = urlopen(
+        "http://api.citybik.es/v2/networks/bikemi"
+    )
+    res = req.read()
+    data = loads(res)
+    return render_template(
+        "citibike.html",
+        company=data["network"]["company"][0],
+        location=data["network"]["location"]["city"]
+    )
+
+
+@app.route("/ricknmorty")
+def ricknmorty():
+    req = urlopen(
+        "https://rickandmortyapi.com/api/character"
+    )
+    res = req.read()
+    data = loads(res)['results']
+    return render_template(
+        "ricknmorty.html",
+        characters = data
+        )
+
+
+@app.route("/collegescorecard")
+def collegescorecard():
+    try:
+        req = urlopen(
+            "https://api.data.gov/ed/collegescorecard/v1/schools?school.name=harvard%20university&api_key=HCoVQQL84LJnQJHkcLlb3p1ZTJn1hVp4AOAnKP1P"
+        )
+        res = req.read()
+        data = loads(res)
+        return render_template(
+            "collegescorecard.html",
+            school=data["results"][0]["school"]["name"],
+            avgsat=data["results"][0]["2017"]["admissions"]["sat_scores"]["average"]["overall"],
+            medact=data["results"][0]["2017"]["admissions"]["act_scores"]["midpoint"]["cumulative"],
+            admitrate=data["results"][0]["2017"]["admissions"]["admission_rate"]["overall"]
+        )
+    except:
+        print("REPLACE YOUR_API_KEY WITH AN ACTUAL API KEY")
+        return render_template(
+            "collegescorecard.html",
+            school="REPLACE YOUR_API_KEY WITH AN ACTUAL API KEY"
+        )
+
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+    app.run(debug=True)

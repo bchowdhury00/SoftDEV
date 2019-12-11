643b437299a285b4bf5621a45e1f30053e611aa1
bchowdhury00 <bchowdhury00@stuy.edu>
2019-12-10 23:57:20 -0500

    m

---
+++ 27_js/funky_town.js
@@ -0,0 +1,40 @@
+/*
+Team prefix -- Manfred Tan, Biraj Chowdhury, and Tammy Chen
+SoftDev1 pd9
+K27 -- Sequential Progression
+2019-12-10
+*/
+
+
+/*
+var foo = function(n){
+  return n;
+};
+*/
+
+var factorial = function(n){
+  if (n == 1) return 1;
+  return (n * factorial(n-1));
+};
+
+
+var fibonacci = function(n){
+  if (n < 2) return n;
+  if (n == 2) return 1;
+  return (fibonacci(n-1) + fibonacci(n-2));
+};
+
+var gcd = function(a,b){
+  if (a < b){
+    return gcd(b, a);
+  }
+  if (a % b == 0) return b;
+  return gcd(a-b, b);
+}
+
+students = ['Manfred', 'Biraj', 'Tammy', 'Fluffy', 'Unicorn']
+
+var randomStudent = function(){
+  var index = Math.floor(Math.random() * students.length);
+  return students[index];
+}

12feb6787d8853a9ec82fc0110b0271f03ac8237
bchowdhury00 <bchowdhury00@stuy.edu>
2020-02-13 20:15:16 -0500

    m

--- 08_canvas-anim/dvd.js
+++ 08_canvas-anim/dvd.js
@@ -4,25 +4,66 @@
 // 2020-02-12
 
 var c = document.getElementById("playground");
-var anim = document.getElementById("animan");
+var animanic = document.getElementById("animan");
 var stop = document.getElementById("stop");
+var dvd = document.getElementById("dvd");
 var ctx = c.getContext('2d');
 
 var animationStarted = false;
+var lastReq = null;
+
 var increase = true;
 var radius = 0;
 
-var circle = function(s){
+var dvdImage = new Image(1,1);
+dvdImage.src = 'dvd.png';
+//state variable to track whether image moves from left to right or right to left
+var moveRight = true;
+var x = 300;
+var y = 400;
+var m = 1.3;
+
+
+var circle = function(x,y,s){
   ctx.beginPath();
-  ctx.arc(300,300, s, 0, 2 * Math.PI);
+  ctx.arc(x,y, s, 0, 2 * Math.PI);
   ctx.stroke();
   ctx.fill();
 }
 
-var lastReq = null;
+var move = function(time){
+    ctx.clearRect(0,0,c.width,c.height);
+    if (x <= 0){
+        moveRight = true;
+        //m = -(1/m);
+    }
+    if (x >= 600){
+        moveRight = false;
+        //m = -(1/m);
+    }
+    if (y <= 0){
+        if (m < 0){
+            m = -(1/m);
+        }
+    }
+    if (y >= 600){
+        if (m > 0){
+            m = -(1/m);
+        }
+    }
+    y += m;
+    if (moveRight){
+        x += 1;
+    }
+    else{
+        x -= 1;
+    }
+    //the last 2 parameters are very important, resizes image to your specifications
+    ctx.drawImage(dvdImage,x - 20,y - 20,60,60);
+    lastReq = requestAnimationFrame(move);
+}
 
 var animate = function(time){
-    animationStarted = true;
     if (radius >= 300){
         increase = false;
     }
@@ -32,23 +73,33 @@ var animate = function(time){
     if (increase){
         ctx.clearRect(0, 0, c.width, c.height);
         radius += 1;
-        circle(radius);
+        circle(300,300,radius);
     }
     else{
         ctx.clearRect(0, 0, c.width, c.height);
         radius -= 1;
-        circle(radius);
+        circle(300,300,radius);
     }
+    //callback function is the parameter for requestAnimationFrame()
     lastReq = window.requestAnimationFrame(animate);
 }
 
-anim.addEventListener("click", function(){
+
+animanic.addEventListener("click", function(){
+    //ensures that multiple clicks on animate button don't speed up animation
 	if (animationStarted == false){
 		animationStarted = true;
 		animate();
 	}
-
 });
+
+dvd.addEventListener("click", function(){
+	if (animationStarted == false){
+		animationStarted = true;
+		move();
+	}
+});
+
 stop.addEventListener("click",function(){
 	animationStarted = false;
 	window.cancelAnimationFrame(lastReq)

0c0a7ea7d194d73e6da8c6c14ff1f804d9c78eb8
bchowdhury00 <bchowdhury00@stuy.edu>
2020-03-17 19:57:28 -0400

    m

---
+++ 11_mongoflask/balldontlie/db.py
@@ -0,0 +1,66 @@
+# Team Blue Jays - Saad Bhuiyan and Biraj Chowdhury
+# SoftDev2 pd9
+# K11 -- Ay Mon Go Git It From Yer Flask
+# 2020-03-07
+
+
+from pymongo import MongoClient
+from bson.json_util import loads
+
+
+def data_to_db(col, file):
+	f = open(file, "r")
+	item = ""
+	for line in f:
+		item += line
+	item = loads(item)
+	col.insert(item)
+	print("added data to MongoDB")
+
+
+def get_players_by_first_name(name, col):
+	results = col.find({"first_name": name})
+	all = []
+	for item in results:
+		all.append(item)
+	return all
+
+
+def get_players_by_last_name(name, col):
+	results = col.find({"last_name": name})
+	all = []
+	for item in results:
+		all.append(item)
+	return all
+
+
+def get_players_by_position(position, col):
+	results = col.find({"position": position})
+	all = []
+	for item in results:
+		all.append(item)
+	return all
+
+
+def get_players_by_team_name(name, col):
+	results = col.find({"team.full_name": name})
+	all = []
+	for item in results:
+		all.append(item)
+	return all
+
+
+def get_players_by_division(division, col):
+	results = col.find({"team.division": division})
+	all = []
+	for item in results:
+		all.append(item)
+	return all
+
+
+def get_players_by_conference(conference, col):
+	results = col.find({"team.conference": conference})
+	all = []
+	for item in results:
+		all.append(item)
+	return all
